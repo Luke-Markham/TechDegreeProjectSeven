909c502fbc43751e9d3c908245160f3c1594f417
Luke Markham <lukeeliotmarkham2@hotmail.com>
2019-05-10 13:07:30 +0700

    eslint

--- web_app_dashboard_v3.1/.eslintrc.js
+++ web_app_dashboard_v3.1/.eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
     "env": {
         "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
+        "es6": true
     },
-    "extends": "airbnb-base",
+    "extends": "google",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
-        "ecmaVersion": 2018
+        "ecmaVersion": 2018,
+        "sourceType": "module"
     },
     "rules": {
     }
